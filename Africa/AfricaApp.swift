706dd9763536921646e1a35ad0e67e9e30fe4f56
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-05 21:45:00 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Necdet Alperen Özil on 5.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
